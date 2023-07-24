27a9a1ca0a10cae493bb5aa1c0af12805cbab614
Modupi <modupimaphoto@gmail.com>
2023-07-24 11:36:31 -0700

    mongodb setup.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+const connection_string = process.env.ATLAS_URI;
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: "mern-stack"
+};
+
+/*
+	mongoose.connect(connection_string, options)
+	.then(() => console.log("Connected..."))
+	.catch((error) => consoole.log(error.message));
+*/
 
 const port = process.env.PORT;
 app.listen(port, () => {
